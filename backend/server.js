fadf4c1cbbb848c494a334115d347cb2160f53f0
Jacob <Jacob.K289@outlook.com>
2023-05-30 01:47:41 +0200

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 
