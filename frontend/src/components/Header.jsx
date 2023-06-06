8bb0071fc89cd9e58c09ded775e3582b4cde9b4c
Jacob <Jacob.K289@outlook.com>
2023-06-07 00:58:32 +0200

    form container added & Login/register screens added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
